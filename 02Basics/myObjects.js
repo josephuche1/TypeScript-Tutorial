6fddeb216463745f95bd7a146ee55b36a932f573
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-25 17:21:24 +0200

    readonly and optional

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,24 +1,11 @@
 "use strict";
+// const User = {
+//     name: 'John',
+//     email: "example@gmail.com",
+//     age: 30,
+//     occupation: 'Software Engineer',
+//     isActive: true,
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-var User = {
-    name: 'John',
-    email: "example@gmail.com",
-    age: 30,
-    occupation: 'Software Engineer',
-    isActive: true,
-};
-var createUser = function (_a) {
-    var string = _a.name, boolean = _a.isPaid;
-};
-var newUser = { name: 'John', isPaid: true, email: "example2gmail.com" };
-createUser(newUser);
-var createCourse = function () {
-    return {
-        name: 'React',
-        price: 100
-    };
-};
-var createUser2 = function (user) {
-    return user;
-};
-createUser2({ name: 'John', email: "example@gmail.com", age: 30, occupation: 'Software Engineer', isActive: true });
+var user = { _id: '123', name: 'John', email: "h@h.com", isActive: true };
+user.email = "example2gmail.com";
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -1,39 +1,65 @@
-const User = {
-    name: 'John',
-    email: "example@gmail.com",
-    age: 30,
-    occupation: 'Software Engineer',
-    isActive: true,
-}
+// const User = {
+//     name: 'John',
+//     email: "example@gmail.com",
+//     age: 30,
+//     occupation: 'Software Engineer',
+//     isActive: true,
+// }
 
-let createUser = ({name: string, isPaid:boolean}) => {};
+// let createUser = ({name: string, isPaid:boolean}) => {};
 
-let newUser = {name: 'John', isPaid: true, email: "example2gmail.com"};
+// let newUser = {name: 'John', isPaid: true, email: "example2gmail.com"};
 
-createUser(newUser);
+// createUser(newUser);
 
-let createCourse = (): {name:string, price: number} => {
-    return{
-        name: 'React',
-        price: 100
-    }
-}
+// let createCourse = (): {name:string, price: number} => {
+//     return{
+//         name: 'React',
+//         price: 100
+//     }
+// }
 
+
+// type User = {
+//     name: string,
+//     email: string,
+//     age: number,
+//     occupation: string,
+//     isActive: boolean,
+// }
+
+// let createUser2 = (user: User) : User => {
+//     return user;
+// }
+
+// type bool = boolean; // type alias - can be used to rename types
+
+// createUser2({name: 'John', email: "example@gmail.com", age: 30, occupation: 'Software Engineer', isActive: true})
 
 type User = {
+    readonly _id: string, // readonly property
     name: string,
     email: string,
-    age: number,
-    occupation: string,
     isActive: boolean,
+    creditCard?: number, // optional property
+}
+let user: User = {_id: '123', name: 'John', email: "h@h.com", isActive: true};
+
+user.email  = "example2gmail.com"; 
+// user._id = "1234"; // error
+
+type cardNumber = {
+    cardNumber: number,
 }
 
-let createUser2 = (user: User) : User => {
-    return user;
+type cardDate = {
+    cardDate: string,
 }
 
-type bool = boolean; // type alias - can be used to rename types
+type cardCVC = {
+    cardCVC: number,
+}
 
-createUser2({name: 'John', email: "example@gmail.com", age: 30, occupation: 'Software Engineer', isActive: true})
+type cardDetails = cardNumber & cardDate & cardCVC; // intersection type
 
 export {};
