694c9e2b15bb4c7a49b2b2ba6c4735a4bfc70335
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-25 17:35:53 +0200

    Transpiled myArray.ts

---
+++ 02Basics/myArray.js
@@ -0,0 +1,10 @@
+var heroes = [];
+// const heroPower: number[] = [];
+var heroPower = []; // another way to declare an array
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255], [1, 2, 3], [4, 5, 6]
+]; // Array of arrays
+heroes.push("Ironman");
+heroPower.push(100);
+allUsers.push({ name: 'John', isActive: true });
