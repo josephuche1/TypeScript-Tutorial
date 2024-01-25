1fbc0b627af8efe649316dab9849d7d83caf9cf2
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-25 13:47:04 +0200

    myObjects.js

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: 'John',
+    email: "example@gmail.com",
+    age: 30,
+    occupation: 'Software Engineer',
+    isActive: true,
+};
+var createUser = function (_a) {
+    var string = _a.name, boolean = _a.isPaid;
+};
+var newUser = { name: 'John', isPaid: true, email: "example2gmail.com" };
+createUser(newUser);
+var createCourse = function () {
+    return {
+        name: 'React',
+        price: 100
+    };
+};
