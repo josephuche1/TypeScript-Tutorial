566fe4f848e21e4122a6c27ba752d1f27ad53a94
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-29 18:32:37 +0200

    Getters and setters private class methods

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,9 +1,38 @@
 "use strict";
+// class User {
+//     email: string;
+//     name: string;
+//     //private proprerties are not accessible outside the class. any property that is not labeled is public by default.
+//     private readonly city: string = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+//     constructor(email:string , name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// A better way to write the above code is to use access modifiers in the constructor parameters.
 class User {
-    constructor(email, name) {
-        this.city = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this._courseCount = 1;
+        this.city = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+    }
+    deleteToken() {
+        console.log("Deleting token");
+    }
+    get getEmail() {
+        return this.email;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(count) {
+        if (count <= 1) {
+            throw new Error("Invalid course count");
+        }
+        this._courseCount = count;
     }
 }
-const joseph = new User("example@gmail.com", "Joseph");
+const joseph = new User("example@gmail.com", "Joseph", "1234");
+// joseph.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,12 +1,48 @@
+// class User {
+//     email: string;
+//     name: string;
+//     //private proprerties are not accessible outside the class. any property that is not labeled is public by default.
+//     private readonly city: string = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+//     constructor(email:string , name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// A better way to write the above code is to use access modifiers in the constructor parameters.
 class User {
-    email: string;
-    name: string;
+    
+    private _courseCount = 1;
     readonly city: string = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
-    constructor(email:string , name: string) {
-        this.email = email;
-        this.name = name;
+    constructor(
+        public email:string , 
+        public name: string,
+        private userId: string
+        ) {
+
+    }
+
+    private deleteToken(){
+        console.log("Deleting token");
+    }
+
+    get getEmail(): string{
+        return this.email;
+    }
+
+    get courseCount(): number{
+        return this._courseCount;
+    }
+
+    set courseCount(count: number){
+        if(count <= 1){
+            throw new Error("Invalid course count");
+        }
+        this._courseCount = count;
     }
 }
 
 
-const joseph = new User("example@gmail.com", "Joseph");
+
+const joseph = new User("example@gmail.com", "Joseph", "1234");
+// joseph.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
