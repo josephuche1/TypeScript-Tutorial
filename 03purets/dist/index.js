7aea6a6b63573b9817e4570eed8a40a06b05f2e6
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-29 17:37:50 +0200

    Classes in TypeScript Create class called User Create constructor and initialize properties create an object of the class User

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
-console.log("Hello World!");
-console.log("Typescript is here!");
-console.log("Watashi wa Typescript desu!");
+class User {
+    constructor(email, name) {
+        this.city = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+        this.email = email;
+        this.name = name;
+    }
+}
+const joseph = new User("example@gmail.com", "Joseph");
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,3 +1,12 @@
-console.log("Hello World!");
-console.log("Typescript is here!");
-console.log("Watashi wa Typescript desu!")
+class User {
+    email: string;
+    name: string;
+    readonly city: string = ""; // You need to initialize the property or use a constructor. You can also make the property readonly.
+    constructor(email:string , name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+
+const joseph = new User("example@gmail.com", "Joseph");
