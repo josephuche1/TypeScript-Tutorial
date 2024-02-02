141dba507604bf227c80501d77167f7b999e9788
PARSIVALJ <uchejesusboy@gmail.com>
2024-02-02 11:24:15 +0200

    Create input UI, Passing functions as props, useState Hook with typescript, reusable todo interface

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
+import {Todo} from "./model"
 
 let name = "joseph";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // console.log(todo);
+
+  const handleAdd= ():void => {
+
+  }
+
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
+      <InputField 
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
     </div>
   );
 }
